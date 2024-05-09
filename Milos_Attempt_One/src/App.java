eff8916c60f21fd66c989834da504f2e9a0bd4a7
Milos Panic <milos.panic@student.tdsb.on.ca>
2024-05-09 13:20:44 -0400

    test for today

--- Milos_Attempt_One/src/App.java
+++ Milos_Attempt_One/src/App.java
@@ -1,5 +1,10 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // create an instance of GuessNumber class
+        // class name object name = new keyword class name
+        GuessNumber guessObject = new GuessNumber();
+        // call the object.method from our class 
+        guessObject.guess();
     }
 }
