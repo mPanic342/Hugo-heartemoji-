22df79a97b7c25c00d756ba9184ef16ca89b065b
Milos Panic <milos.panic@student.tdsb.on.ca>
2024-05-10 14:14:53 -0400

    altered code

--- Milos_Attempt_One/src/MagicBall.java
+++ Milos_Attempt_One/src/MagicBall.java
@@ -7,30 +7,35 @@ public class MagicBall {
         // declaring variables
         String message = "Do you have any questions for the magical and mysterious 8-ball that will forever control your life?";
         String title = "MAGIC 8-BALL";
-        String input =  (JOptionPane.showInputDialog(message, title));
+        String input = (JOptionPane.showInputDialog(message, title));
         // declaring word bank
-        String[] responses = {
-            "It is certain.",
-            "It is decidedly so.",
-            "Without a doubt.",
-            "Yes - definitely.",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not to tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful."
-        };
+        if (input != null && !input.trim().isEmpty()) {
+            Random random = new Random();
+            String[] responses = {
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not to tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."
+            };
+         int randomIndex = random.nextInt(responses.length);
+            String randomResponse = responses[randomIndex];
         
     };
+}
 }
