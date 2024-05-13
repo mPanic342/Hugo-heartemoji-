a2b6a3e358e631b21a7ab343b87f87e87867e0dd
Milos Panic <milos.panic@student.tdsb.on.ca>
2024-05-13 13:55:30 -0400

    JOptionPane.showInputDialog is not complete, commenting is not complete. remove resized icon from input and it will work.

--- Milos_Attempt_One/src/MagicBall.java
+++ Milos_Attempt_One/src/MagicBall.java
@@ -1,10 +1,23 @@
+// importing libraries
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+import java.awt.Component;
+import java.awt.Image;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
+
 public class MagicBall {
-    public void guess(){
+    public void guess() throws MalformedURLException{ // main method
+       
+        ImageIcon originalIcon = new ImageIcon(new URL("https://upload.wikimedia.org/wikipedia/commons/9/90/Magic8ball.jpg")); // retrieving image url
+        Image image = originalIcon.getImage().getScaledInstance(32, 32, Image.SCALE_DEFAULT); // scaling image
+        ImageIcon resizedIcon = new ImageIcon(image);
+
         String message = "Do you have any questions for the magical and mysterious 8-ball that will forever control your life?";
         String title = "Input question here";
-        String input = (JOptionPane.showInputDialog(message, title));
+        String input = (JOptionPane.showInputDialog(message, title, JOptionPane.INFORMATION_MESSAGE, resizedIcon));
+
         if (input != null && !input.trim().isEmpty()) {
             Random random = new Random();
             String[] responses = {
@@ -32,8 +45,8 @@ public class MagicBall {
             
         int randomIndex = random.nextInt(responses.length);
         String randomResponse = responses[randomIndex];
-            
-        JOptionPane.showMessageDialog(null, randomResponse, "MAGIC 8-BALL RESPONSE", JOptionPane.INFORMATION_MESSAGE);
+        
+        JOptionPane.showMessageDialog(null, randomResponse, "MAGIC 8-BALL RESPONSE", JOptionPane.INFORMATION_MESSAGE, resizedIcon);
             
         }
     };
